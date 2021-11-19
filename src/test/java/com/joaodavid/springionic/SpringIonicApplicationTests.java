82969a3278e8cdbb5acdc81bdc7136a32d017d9f
Jdavid77 <jdavid72009@hotmail.com>
2021-11-19 16:08:24 +0000

    Project Created

---
+++ src/test/java/com/joaodavid/springionic/SpringIonicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaodavid.springionic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIonicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

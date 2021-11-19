cde327ff3af8f8906ad4ab0ff8763b6c8a44a625
Jdavid77 <jdavid72009@hotmail.com>
2021-11-19 16:45:55 +0000

    H2 Database

--- src/main/java/com/joaodavid/springionic/model/Categoria.java
+++ src/main/java/com/joaodavid/springionic/model/Categoria.java
@@ -3,11 +3,21 @@ package com.joaodavid.springionic.model;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_categorias")
 public class Categoria implements Serializable{
 
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
 	

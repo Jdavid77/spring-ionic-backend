a75a016c3579d8e1249bb8aa32e706e57e5041ec
Jdavid77 <jdavid72009@hotmail.com>
2021-11-19 16:30:44 +0000

    Testing Category

--- src/main/java/com/joaodavid/springionic/resources/CategoriaResource.java
+++ src/main/java/com/joaodavid/springionic/resources/CategoriaResource.java
@@ -1,15 +1,24 @@
 package com.joaodavid.springionic.resources;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.joaodavid.springionic.model.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
 	@GetMapping
-	public String Listar() {
-		return "REST está funcionando";
+	public ResponseEntity<List<Categoria>> Listar() {
+		
+		Categoria cat1 = new Categoria(1,"Informática");
+		Categoria cat2 = new Categoria(2,"Escritório");
+		return ResponseEntity.ok().body(Arrays.asList(cat1,cat2)) ;
 	}
 }

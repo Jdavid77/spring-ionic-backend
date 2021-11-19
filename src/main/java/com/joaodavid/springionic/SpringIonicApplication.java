48d180181dc28249a896383a5a55b5331cd76b51
Jdavid77 <jdavid72009@hotmail.com>
2021-11-19 21:41:34 +0000

    Product and ManyToMany Relation

--- src/main/java/com/joaodavid/springionic/SpringIonicApplication.java
+++ src/main/java/com/joaodavid/springionic/SpringIonicApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.joaodavid.springionic.model.Categoria;
+import com.joaodavid.springionic.model.Produto;
 import com.joaodavid.springionic.repository.CategoriaRepository;
+import com.joaodavid.springionic.repository.ProdutoRepository;
 
 @SpringBootApplication
 public class SpringIonicApplication implements CommandLineRunner{
 
 	@Autowired
 	private CategoriaRepository categoriarepository;
+	
+	@Autowired
+	private ProdutoRepository produtorepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringIonicApplication.class, args);
@@ -26,8 +31,19 @@ public class SpringIonicApplication implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null,"Informática");
 		Categoria cat2 = new Categoria(null,"Escritório");
 		
-		categoriarepository.saveAll(Arrays.asList(cat1,cat2));
+		Produto p1 = new Produto(null,"Computador",2000.0);
+		Produto p2 = new Produto(null,"Impressora",800.0);
+		Produto p3 = new Produto(null,"Mouse",80.0);
 		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		categoriarepository.saveAll(Arrays.asList(cat1,cat2));
+		produtorepository.saveAll(Arrays.asList(p1,p2,p3));
 	}
 	
 	
--- src/main/java/com/joaodavid/springionic/model/Categoria.java
+++ src/main/java/com/joaodavid/springionic/model/Categoria.java
@@ -1,12 +1,15 @@
 package com.joaodavid.springionic.model;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -20,6 +23,10 @@ public class Categoria implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
+	
+	
+	@ManyToMany(mappedBy = "categorias")
+	private List<Produto> produtos = new ArrayList<>();
 	
 	public Categoria() {
 		
@@ -45,6 +52,15 @@ public class Categoria implements Serializable{
 
 	public void setName(String name) {
 		this.name = name;
+	}
+	
+
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
 	}
 
 	@Override

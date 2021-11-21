c9e998e9cecc498add954a03b062b3bdb19ad32f
Jdavid77 <jdavid72009@hotmail.com>
2021-11-21 18:06:38 +0000

    Insert Category POST method

--- src/main/java/com/joaodavid/springionic/resources/CategoriaResource.java
+++ src/main/java/com/joaodavid/springionic/resources/CategoriaResource.java
@@ -1,11 +1,18 @@
 package com.joaodavid.springionic.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.joaodavid.springionic.model.Categoria;
 import com.joaodavid.springionic.service.CategoriaService;
@@ -21,5 +28,13 @@ public class CategoriaResource {
 	public ResponseEntity<?> findById(@PathVariable Integer id) {
 		Categoria obj = service.findById(id);
 		return ResponseEntity.ok().body(obj) ;
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+		obj = service.Insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
--- src/main/java/com/joaodavid/springionic/service/CategoriaService.java
+++ src/main/java/com/joaodavid/springionic/service/CategoriaService.java
@@ -1,9 +1,11 @@
 package com.joaodavid.springionic.service;
 
+import java.net.URI;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.joaodavid.springionic.model.Categoria;
 import com.joaodavid.springionic.repository.CategoriaRepository;
@@ -19,6 +21,11 @@ public class CategoriaService {
 		Optional<Categoria> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+	
+	public Categoria Insert(Categoria obj) {
+		obj.setId(null);
+		return repository.save(obj);
 	}
 	
 	

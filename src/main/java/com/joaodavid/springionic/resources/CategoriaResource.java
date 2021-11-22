a7d858a5ecc73da0b337d9854e441804b693e4e6
Jdavid77 <jdavid72009@hotmail.com>
2021-11-22 11:40:57 +0000

    Personalizaded Validation

--- src/main/java/com/joaodavid/springionic/resources/CategoriaResource.java
+++ src/main/java/com/joaodavid/springionic/resources/CategoriaResource.java
@@ -47,7 +47,8 @@ public class CategoriaResource {
 	}
 	
 	@PutMapping(value = "/{id}")
-	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id){
+	public ResponseEntity<Void> update(@Valid @RequestBody CategoriaDTO objDTO, @PathVariable Integer id){
+		Categoria obj = service.fromDTO(objDTO);
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();

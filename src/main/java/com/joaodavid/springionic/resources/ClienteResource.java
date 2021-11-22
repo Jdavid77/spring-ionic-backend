8950149eb1293f55d66334c9732ab4c523ab6903
Jdavid77 <jdavid72009@hotmail.com>
2021-11-22 16:51:06 +0000

    PUT DELETE AND GET CLIENT

--- src/main/java/com/joaodavid/springionic/resources/ClienteResource.java
+++ src/main/java/com/joaodavid/springionic/resources/ClienteResource.java
@@ -1,12 +1,23 @@
 package com.joaodavid.springionic.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.joaodavid.springionic.DTO.ClienteDTO;
 import com.joaodavid.springionic.model.Cliente;
 import com.joaodavid.springionic.service.ClienteService;
 
@@ -21,5 +32,37 @@ public class ClienteResource {
 	public ResponseEntity<?> findById(@PathVariable Integer id) {
 		Cliente obj = service.findById(id);
 		return ResponseEntity.ok().body(obj) ;
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDTO, @PathVariable Integer id){
+		Cliente obj = service.fromDTO(objDTO);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping
+	public ResponseEntity<?> findAll() {
+		List<Cliente> list = service.findAll();
+		List<ClienteDTO> listDTO = list.stream().map(x -> new ClienteDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO) ;
+	}
+	
+	@GetMapping(value = "/page")
+	public ResponseEntity<?> findPage(
+			@RequestParam(value="page",defaultValue = "0") Integer page, 
+			@RequestParam(value="linesPerPage",defaultValue = "24") Integer linesPerPage,
+			@RequestParam(value="orderBy",defaultValue = "nome") String orderBy,
+			@RequestParam(value="direction",defaultValue = "ASC") String direction) {
+		Page<Cliente> list = service.findPage(page,linesPerPage,orderBy,direction);
+		Page<ClienteDTO> listDTO = list.map(x -> new ClienteDTO(x));
+		return ResponseEntity.ok().body(listDTO) ;
 	}
 }

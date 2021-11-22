048219d349bd05025bb5dfc9ad593af60c64dab3
Jdavid77 <jdavid72009@hotmail.com>
2021-11-22 21:54:53 +0000

    POST Cliente

--- src/main/java/com/joaodavid/springionic/resources/ClienteResource.java
+++ src/main/java/com/joaodavid/springionic/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package com.joaodavid.springionic.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,13 +12,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.joaodavid.springionic.DTO.CategoriaDTO;
 import com.joaodavid.springionic.DTO.ClienteDTO;
+import com.joaodavid.springionic.DTO.ClienteNewDTO;
+import com.joaodavid.springionic.model.Categoria;
 import com.joaodavid.springionic.model.Cliente;
 import com.joaodavid.springionic.service.ClienteService;
 
@@ -32,6 +38,15 @@ public class ClienteResource {
 	public ResponseEntity<?> findById(@PathVariable Integer id) {
 		Cliente obj = service.findById(id);
 		return ResponseEntity.ok().body(obj) ;
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDTO){
+		Cliente obj = service.fromDTO(objDTO);
+		obj = service.Insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	
 	@PutMapping(value = "/{id}")
--- src/main/java/com/joaodavid/springionic/service/ClienteService.java
+++ src/main/java/com/joaodavid/springionic/service/ClienteService.java
@@ -11,9 +11,13 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.joaodavid.springionic.DTO.ClienteDTO;
+import com.joaodavid.springionic.DTO.ClienteNewDTO;
+import com.joaodavid.springionic.model.Cidade;
 import com.joaodavid.springionic.model.Cliente;
-import com.joaodavid.springionic.model.Cliente;
+import com.joaodavid.springionic.model.Endereco;
+import com.joaodavid.springionic.model.enums.TipoCliente;
 import com.joaodavid.springionic.repository.ClienteRepository;
+import com.joaodavid.springionic.repository.EnderecoRepository;
 import com.joaodavid.springionic.service.exceptions.DataIntegrityException;
 import com.joaodavid.springionic.service.exceptions.ObjectNotFoundException;
 
@@ -23,11 +27,23 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository repository;
 	
+	@Autowired
+	private EnderecoRepository enderecorepository;
+	
 	public Cliente findById(Integer id) {
 		Optional<Cliente> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 	}
+	
+	
+	public Cliente Insert(Cliente obj) {
+		obj.setId(null);
+		obj = repository.save(obj);
+		enderecorepository.saveAll(obj.getEnderecos());
+		return obj;
+	}
+	
 	
 	public Cliente update(Cliente obj) {
 		Cliente newObj = findById(obj.getId());
@@ -57,8 +73,26 @@ public class ClienteService {
 		return new Cliente(objDTO.getId(),objDTO.getNome(),objDTO.getEmail(),null,null);
 	}
 	
+	
+	public Cliente fromDTO(ClienteNewDTO objDTO) {
+		Cliente cli = new Cliente(null,objDTO.getNome(),objDTO.getEmail(),objDTO.getCpfOuCnpj(),TipoCliente.toEnum(objDTO.getTipo()));
+		Cidade cid = new Cidade(objDTO.getCidadeId(),null,null);
+		Endereco end = new Endereco(null,objDTO.getLogradouro(),objDTO.getNumero(),objDTO.getComplemento(),objDTO.getBairro(),objDTO.getCep(),cli,cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDTO.getTelefone1());
+		if(objDTO.getTelefone2() != null) {
+			cli.getTelefones().add(objDTO.getTelefone2());
+		}
+		if(objDTO.getTelefone3() != null) {
+			cli.getTelefones().add(objDTO.getTelefone3());
+		}
+		return cli;
+	}
+	
 	private void updateData(Cliente newObj,Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
 	}
+	
+	
 }

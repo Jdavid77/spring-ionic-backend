95ee79b05a342b43ba26a8541967d394afc08d24
Jdavid77 <jdavid72009@hotmail.com>
2021-11-24 18:45:33 +0000

    Create test profile

---
+++ src/main/java/com/joaodavid/springionic/config/TestConfig.java
@@ -0,0 +1,25 @@
+package com.joaodavid.springionic.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.joaodavid.springionic.service.DBService;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Bean
+	public boolean InstantiateDatabase() throws ParseException {
+		dbService.InstantiateTestDatabase();
+		return true;
+	}
+}
